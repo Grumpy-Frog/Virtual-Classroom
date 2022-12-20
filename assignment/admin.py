55b46adab8d1a6a7e48c3b310adbeedfa29415b0
Rafi Hassan Chowdhury <64986440+Grumpy-Frog@users.noreply.github.com>
2022-12-20 20:15:08 +0600

    Delete assignment directory

--- assignment/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Assignment, AssignmentSubmission
-
-# Register your models here.
-
-admin.site.register(Assignment)
-admin.site.register(AssignmentSubmission)
--- assignment/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Assignment
-
-
-class AssignmentForm(forms.ModelForm):
-    class Meta:
-        fields = ("title", "description", "due", "points")
-        model = Assignment
