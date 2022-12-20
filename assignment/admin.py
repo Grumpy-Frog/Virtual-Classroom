d59e3280dfb38f379c41393da0d438552ebd4239
Rafi_Hassan <rafihassan@iut-dhaka.edu>
2022-12-20 20:35:22 +0600

    Final Merge

--- assignment/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Assignment, AssignmentSubmission,AssignmentComment
-
-# Register your models here.
-
-admin.site.register(Assignment)
-admin.site.register(AssignmentSubmission)
-admin.site.register(AssignmentComment)
